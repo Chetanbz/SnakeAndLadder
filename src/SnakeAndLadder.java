504acd351a4deafda531f758154bc3cbe932046e
chetan <zambare.c123@gmail.com>
2020-11-21 12:50:14 +0530

    Throw a die

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -4,6 +4,8 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		System.out.println("Please start the game ");
 		int start = 0;  /// We are initializing player 1 position as 0
+		int dieNum = (int)Math.floor(Math.random()*10)%6 + 1;
+		System.out.println(dieNum);
 	}
 
 }
