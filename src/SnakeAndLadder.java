2a8042f3241739896a59acb00a47a913d80fe4d4
chetan <zambare.c123@gmail.com>
2020-11-21 12:46:30 +0530

    Initialize player 1 position as 0

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -3,7 +3,7 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Please start the game ");
-		
+		int start = 0;  /// We are initializing player 1 position as 0
 	}
 
 }
